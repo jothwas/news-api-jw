a66b678f076c214b5843a327c7e8abeace6a5696
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-02-16 12:33:18 +0000

    amend to tests and run-seed.js  following PR comments

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -3,15 +3,8 @@ const testData = require("../data/test-data/index.js");
 const seed = require("./seed.js");
 const db = require("../connection.js");
 
-const ENV = process.env.NODE_ENV || "development";
-
-const data = {
-  test: testData,
-  development: devData,
-};
-
 const runSeed = () => {
-  return seed(data[ENV]).then(() => db.end());
+  return seed(testData).then(() => db.end());
 };
 
 runSeed();
