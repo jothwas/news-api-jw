231f6e5206fccc17141a22660446ed8a9834fdb6
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-02-19 23:03:52 +0000

    add userRouter, refactor app.js

--- app/app.js
+++ app/app.js
@@ -1,32 +1,16 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("../controllers/topics-controllers");
-const {
-  getArticlesById,
-  patchArticlesById,
-  getAllArticles,
-} = require("../controllers/articles-controllers");
 const {
   error404,
   psqlErrorCodes,
   customErrors,
   serverErrors,
 } = require("../errors/errors");
-const { getUsers } = require("../controllers/users-controllers");
-const {
-  getCommentsByArticleId,
-  postCommentsByArticleId,
-  deleteCommentsById,
-} = require("../controllers/comments-controllers");
-const { getEndpoints } = require("../controllers/endpoint-request-controllers");
 const apiRouter = require("../routes/api-routers");
+
 app.use(express.json());
-
-app.get("/api/users", getUsers);
-
-/////// ERRORS
-
 app.use("/api", apiRouter);
+
 app.all("/*", error404);
 app.use(customErrors);
 app.use(psqlErrorCodes);
--- routes/api-routers.js
+++ routes/api-routers.js
@@ -2,6 +2,7 @@ const { getEndpoints } = require("../controllers/endpoint-request-controllers");
 const articlesRouter = require("./article-routers");
 const commentsRouter = require("./comment-routers");
 const topicRouter = require("./topic-routers");
+const userRouter = require("./user-routers");
 
 const apiRouter = require("express").Router();
 
@@ -9,5 +10,6 @@ apiRouter.route("/").get(getEndpoints);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
 apiRouter.use("/topics", topicRouter);
+apiRouter.use("/users", userRouter);
 
 module.exports = apiRouter;
